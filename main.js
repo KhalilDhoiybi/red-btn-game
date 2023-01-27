85279772ef60297c87a8edfab7426abc52c5d8fc
KhalilDhoiybi <kkhalildoiybi@gmail.com>
2023-01-27 21:58:03 +0000

    colored btns func

--- main.js
+++ main.js
@@ -19,5 +19,14 @@ var redButtons = document.getElementsByClassName("red-btn");
 for (let i = 0; i < 4; i++) {
   redButtons[i].addEventListener("mouseover", () => {
     redButtons[i].className = "btn red-btn red-btn-move";
+    if (i == 0) {
+      document.getElementById("green-btn").className = "btn hidden-btn-show";
+    } else if (i == 1) {
+      document.getElementById("yellow-btn").className = "btn hidden-btn-show";
+    } else if (i == 2) {
+      document.getElementById("blue-btn").className = "btn hidden-btn-show";
+    } else {
+      document.getElementById("purple-btn").className = "btn hidden-btn-show";
+    }
   });
 }
